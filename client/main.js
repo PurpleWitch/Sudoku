3ed023220e760800807b4863a8b2a7aabd3d0e09
RamiOkkeh <ramiokkeh@gmail.com>
2021-02-27 23:39:12 +0200

    z

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
 
-import './main.html';
+import "./main.html";
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  this.counter = new ReactiveVar(10);
 });
 
 Template.hello.helpers({
@@ -15,7 +15,7 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
+  "click button"(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
