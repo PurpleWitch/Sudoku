c5e4baf4919812f643c7213eeb08092d6be836a7
hamdalla2 <hamdalla2@yahoo.com>
2021-03-12 14:17:50 +0200

    .

--- client/app.module.ts
+++ client/app.module.ts
@@ -1,13 +1,12 @@
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   bootstrap: [
-    IonicApp
+    AppComponent
   ],
   declarations: [
     AppComponent
@@ -18,16 +17,12 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    IonicModule.forRoot(AppComponent),
     RouterModule.forRoot([
       // { path: '', redirectTo: '/signin', pathMatch: 'full'},
       // { path: 'Signin', component: signin },
       // { path: 'Signup', component: signup },
       // { path: '**', component: error }
     ])
-  ],
-  providers: [
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 
